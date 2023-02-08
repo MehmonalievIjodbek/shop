cae17bdf0a8a7a45b9e2343fdedfb665878a479c
MehmonalievIjodbek <mehmonaliyevijodbek@gmail.com>
2023-02-08 18:35:53 +0500

    shop project shop

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="page-footer">
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()}
+          <a className="grey-text text-lighten-4 right" href="#!">
+            REPO    
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const API_URL = " https://fortniteapi.io/v1/shop?lang=en";
+const API_KEY = "376beea5-d0f93c61-f2ad5f91-c3310e7d";
+
+export { API_KEY, API_URL };
